3c8148ad7841941243f0d6327fa62e5872807be6
Patchanan <patchanansnt@gmail.com>
2025-11-25 04:03:37 +0700

    -

--- Frontend/.next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++ Frontend/.next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
@@ -1,7 +1,14 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__28bc9c2a._.css"
+  "static/chunks/_fb0d7dd2._.css",
+  "static/chunks/node_modules_bf62ca14._.js",
+  "static/chunks/node_modules_@rainbow-me_rainbowkit_dist_05c5dc18._.js",
+  "static/chunks/node_modules_@wagmi_core_dist_esm_abcf7b20._.js",
+  "static/chunks/node_modules_viem__esm_3f44557f._.js",
+  "static/chunks/node_modules_@noble_curves_esm_b805058c._.js",
+  "static/chunks/node_modules_ox__esm_91300269._.js",
+  "static/chunks/node_modules_4ceddb25._.js"
 ],
     source: "dynamic"
 });
